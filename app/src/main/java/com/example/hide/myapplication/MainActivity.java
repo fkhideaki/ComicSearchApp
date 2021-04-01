962111dfc62702154c0350ec052cf00d8c4fe6ce
qw_fuku <fkhideaki@gmail.com>
2021-04-02 01:31:50 +0900

    検索機能実装

--- app/src/main/java/com/example/hide/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hide/myapplication/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.hide.myapplication;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableStringBuilder;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,8 +16,29 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private enum ShopType
+    {
+        M,A,T,G,
+    }
+
+    private class SearchCmd implements View.OnClickListener
+    {
+        ShopType st;
+
+        public SearchCmd(ShopType st_)
+        {
+            st = st_;
+        }
+
+        @Override
+        public void onClick(View v) {
+            onSearch(st);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +71,80 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button btnSearchM = (Button) findViewById(R.id.buttonSearchM);
+        Button btnSearchA = (Button) findViewById(R.id.buttonSearchA);
+        Button btnSearchT = (Button) findViewById(R.id.buttonSearchT);
+        Button btnSearchG = (Button) findViewById(R.id.buttonSearchG);
+        btnSearchM.setOnClickListener(new SearchCmd(ShopType.M));
+        btnSearchA.setOnClickListener(new SearchCmd(ShopType.A));
+        btnSearchT.setOnClickListener(new SearchCmd(ShopType.T));
+        btnSearchG.setOnClickListener(new SearchCmd(ShopType.G));
+
         setupWebview();
+    }
+
+    private void onSearch(ShopType st) {
+        WebView wv = (WebView) findViewById(R.id.webView);
+        EditText t = (EditText) findViewById(R.id.editSearch);
+        SpannableStringBuilder sb = (SpannableStringBuilder)t.getText();
+        String w = sb.toString();
+        String url = makeURL(w, st);
+        wv.loadUrl(url);
+    }
+
+    private String makeURL(String t, ShopType st) {
+        switch (st)
+        {
+            case M: return makeUrlM(t);
+            case T: return makeUrlT(t);
+            case A: return makeUrlA(t);
+            case G: return makeUrlG(t);
+            default: return null;
+        }
+    }
+
+    private String makeUrlM(String w) {
+        int mode = 2;
+        String s = "https://www.melonbooks.co.jp/search/search.php";
+        s += "?mode=search";
+        s += "&search_disp=";
+        s += "&category_id=0";
+        s += "&text_type=";
+        s += "&text_type=all";
+        s += "&name=" + w;
+        if (mode == 1)
+            s += "&category_ids%5B%5D=1";
+        else if (mode == 2)
+            s += "&category_ids%5B%5D=4";
+
+        return s;
+    }
+
+    private String makeUrlT(String w) {
+        int mode = 2;
+        String s = "https://ec.toranoana.shop/tora/ec/app/catalog/list/";
+        s += "?searchDisplay=0";
+        s += "&searchBackorderFlg=1";
+        s += "&searchWord=" + w;
+        if (mode == 1)
+            s += "&commodity_kind_name=同人誌";
+        else if (mode == 2)
+            s += "&commodity_kind_name=書籍";
+        return s;
+    }
+
+    private String makeUrlA(String w) {
+        String s = "https://www.animate-onlineshop.jp/products/list.php";
+        s += "?mode=search";
+        s += "&smt=" + w;
+        return s;
+    }
+
+    private String makeUrlG(String w) {
+        String s = "https://www.gamers.co.jp/products/list.php";
+        s += "?mode=search";
+        s += "&smt=" + w;
+        return s;
     }
 
     private void onBtnBackClicked() {
@@ -57,8 +153,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onBtnTopClicked() {
-        WebView wv = (WebView) findViewById(R.id.webView);
-        wv.loadUrl("http://fkhd.php.xdomain.jp/comicSearch/");
     }
 
     private void setupWebview() {
@@ -68,7 +162,6 @@ public class MainActivity extends AppCompatActivity {
         s.setLoadWithOverviewMode(true);
         s.setUseWideViewPort(true);
         s.setJavaScriptEnabled(true);
-        wv.loadUrl("http://fkhd.php.xdomain.jp/comicSearch/");
     }
 
     private void showMsgBoxTest() {
